bf458694a72ed8d6b916148eacde49af4f3abd7b
bagashiz <work.bitsy@simplelogin.com>
2023-01-13 21:58:12 +0700

    add NPM

---
+++ 09_NPM/index.js
@@ -0,0 +1,8 @@
+//* Importing a module
+// use _ is a standard convention for lodash
+import _ from "lodash";
+
+const myArray = [1, 2, 3, 4];
+let sum = _.sum(myArray); // use lodash to sum the array
+
+console.log(sum); // 10
